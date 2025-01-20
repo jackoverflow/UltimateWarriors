2e8881a39724d9decbed44296936637d24e65d59
Leeyo Rapirap <leorapirap@gmail.com>
2025-01-20 11:32:06 +0800

    Install react-router-dom for routing functionality

--- ultimatewarriors.client/src/App.jsx
+++ ultimatewarriors.client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
     const [forecasts, setForecasts] = useState();
@@ -8,34 +9,13 @@ function App() {
         populateWeatherData();
     }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
     return (
         <div>
-            <h1 id="tableLabel">Weather forecast</h1>
+            <h1 id="tableLabel">Weather Forecast</h1>
             <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+            <p>
+                <Link to="/create-weapon">Create a New Weapon</Link>
+            </p>
         </div>
     );
     
