e9884f9ffddfeb4a8273272731c6426f02a16202
Leeyo Rapirap <leorapirap@gmail.com>
2025-01-21 12:24:09 +0800

    Add Weapon model: Created CreateWarriorWithWeaponRequest class to represent weapon entities in the application.

--- UltimateWarriors.Server/Models/CreateWarriorWithWeaponsRequest.cs
+++ UltimateWarriors.Server/Models/CreateWarriorWithWeaponsRequest.cs
@@ -1,9 +1,9 @@
 namespace UltimateWarriors.Server.Models
 {
-    public class Weapon
+    public class CreateWarriorWithWeaponsRequest
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<int> WeaponIds { get; set; }
     }
-} 
+}
